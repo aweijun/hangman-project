33d233ca408f8c27845bad372f8a5b7c01b4fe59
aweijun <weijun.ang99@gmail.com>
2022-05-18 19:06:55 +0800

    Create Keyboard for user input

--- src/pages/GameScreen.js
+++ src/pages/GameScreen.js
@@ -1,21 +1,32 @@
-import { Button } from '@chakra-ui/react'
+import { Button, Flex, Heading, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
 
 function GameScreen() {
-  const [count, setCount] = useState(0)
-  const [guesses, setGuesses] = useState([])
+  const [count, setCount] = useState(0) //This is the number of tries
+  const [guesses, setGuesses] = useState([]) //This is the list of the given keyboard
   const [word, setWord] = useState(["Hello"])
+
+  const LETTERS = "abcdefghjklmnopqrstuvwxyz"
 
   let handleInput = (e) => {
     console.log(e.target.value)
+    setCount(count + 1)
   }
 
   return (
-    <div>GameScreen
-    <Button key={'a'} value={'b'} onClick={handleInput}>
-      Hi
-    </Button>
-    </div>
+    <VStack>
+    <Heading>
+      GameScreen
+    </Heading>
+    //Display
+    <Flex>
+    {LETTERS.split("").map(letter => (
+      <Button value={letter} onClick={handleInput}>
+        {letter}
+      </Button>
+    ))}
+    </Flex>
+    </VStack>
   )
 }
 
---
+++ src/pages/components/UserInput.js
@@ -0,0 +1,21 @@
+import { Button } from '@chakra-ui/react'
+import React from 'react'
+
+function UserInput(props) {
+
+  const USER_STATE  = {
+    noTry: 0,
+    wrongTry: 1,
+    rightTry: 2,
+  }
+
+  const letter = props.letter
+
+  return (
+    <Button>
+
+    </Button>
+  )
+}
+
+export default UserInput
